126692519e854fe307186572d3c16e55c51d706f
Aluno <thayna.matos.carlos@gmail.com>
2025-05-23 13:17:15 -0300

    configurações total e menu de adicionar e retirar itens

--- App.js
+++ App.js
@@ -7,10 +7,15 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   return (
+
+    // BACKGROUND DO APP
+
     <View style={styles.container}>
       <ImageBackground source={require("./src/assets/img/HqCola.jpg")} style={{
-        width: "100%", height: "80%", resizeMode: "cover"
+        width: "100%", height: "80%", resizeMode: "cover",
       }} >
+
+        {/* BOTAO DE VOLTAR NO MENU PRINCIPAL */}
 
         <TouchableOpacity style={{
           top: 60,
@@ -26,6 +31,9 @@ export default function App() {
         </TouchableOpacity>
 
       </ImageBackground>
+
+      {/* DIV QUE ENGLOBA O NOME DO PRODUTO E UM COMENTARIOZINHO ABAIXO */}
+
       <View style={{
         position: "absolute",
         bottom: 0,
@@ -34,23 +42,116 @@ export default function App() {
         height: "50%",
         backgroundColor: "white",
         borderTopRightRadius: 50,
-        padding: 30
       }}>
-        <Text style={{ bottom: 5, color: "black", }}>COCA COLA 300ml</Text>
-        <Text style={{ color: "gray", }}>EDIÇÃO LIMITADA VINGADORES</Text>
+
+
+        <Text style={{ bottom: 5, color: "black", fontSize: 30, }}>COCA COLA 300ml</Text>
+        <Text style={{ color: "gray", fontStyle: "italic" }}>EDIÇÃO LIMITADA VINGADORES</Text>
+
+        {/* BOTAO DE ADIOCINAR OU DIMINUIR PRODUTOS */}
+
         <View style={{
-          width: 80,
-          padding: 10,
-          borderWidth: 1,
-          borderColor: "red",
-          borderRadius: 10,
+          position: "absolute",
+          right: 40,
+          top: -40,
+          backgroundColor: "gray",
+          width: 50,
           justifyContent: "center",
           alignItems: "center",
-          top: 10,
+          padding: 5,
+          borderRadius: 25,
+          gap: 10,
         }}>
-          <Text style={{ color: "red" }}>100%</Text>
-          <Text style={{ color: "red" }}>Mimimi</Text>
+
+          <TouchableOpacity style={{
+            width: 20,
+            height: 20,
+            backgroundColor: "gray",
+            borderRadius: 20,
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+
+            <Text style={{ color: "black" }}> + </Text>
+
+          </TouchableOpacity>
+
+          <View style={{ backgroundColor: "#c21313", height: 30, width: 30, borderRadius: 30, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{
+              color: "black",
+            }}> 2 </Text>
+          </View>
+          <TouchableOpacity style={{
+            width: 20,
+            height: 20,
+            backgroundColor: "gray",
+            borderRadius: 20,
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+            <Text style={{ color: "black" }}> - </Text>
+          </TouchableOpacity>
         </View>
+
+        {/* CATEGORIAS DO PRODUTO */}
+
+        <View style={{ width: "100%", marginTop: 10, gap: 20, flexDirection: "row" }}>
+          <View style={{
+            padding: 10,
+            borderWidth: 1,
+            borderColor: "#c21313",
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+            <Text style={{ color: "#c21313" }}>0,0001%</Text>
+            <Text style={{ color: "#c21313" }}>Bondoso</Text>
+          </View>
+
+          <View style={{
+            padding: 10,
+            borderWidth: 1,
+            borderColor: "#c21313",
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+            <Text style={{ color: "#c21313" }}>99,9999%</Text>
+            <Text style={{ color: "#c21313" }}>Louco</Text>
+          </View>
+
+          <View style={{
+            padding: 10,
+            borderWidth: 1,
+            borderColor: "#c21313",
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+            <Text style={{ color: "#c21313" }}>ꝏ</Text>
+            <Text style={{ color: "#c21313" }}>Foda</Text>
+          </View>
+        </View>
+
+        {/* PREÇO DO PRODUTO UNICO E A QUANTIDADE QUE ESTA NO BOTAO DE AUMENTAR E DIMINUIR O PRODUTO */}
+
+        <View style={{ width: "100%", gap: 20, flexDirection: "row", marginTop: 20, justifyContent: "center" }}>
+          <View style={{ width: "40%", backgroundColor: "#c21313", padding: 15, borderRadius: 10 }}>
+            <Text style={{ color: "white" }}>R$3,90 a unidade</Text>
+          </View>
+
+          <View style={{ width: "40%", backgroundColor: "#c21313", padding: 15, borderRadius: 10 }}>
+            <Text style={{ color: "white" }}>R$7,80 reais <Text style={{ color: "white" }}>2 unidades</Text> </Text>
+          </View>
+        </View>
+
+        {/* BOTAO NA QUAL FALA O TOTAL! */}
+
+        <View style={{ flex: 1, backgroundColor: "gray", padding: 30 }}>
+          <Text style={{ color: "black", fontSize: 18, marginTop: 20, }}>Total</Text>
+        </View>
+
+
       </View>
     </View>
   );
