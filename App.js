8484cb2590d8e39f65dc39dfc40b3e6ebe746a58
Aluno <thayna.matos.carlos@gmail.com>
2025-05-23 15:30:27 -0300

    Ultimos Detalhes

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 
+// IMPORTAÇÃO USESTATE variavél que muda a quantidade automaticamente pra gente.
+import { useState } from 'react';
+
+// IMPORTAÇÃO STYLES
 import { styles } from './src/assets/styles/styles';
 
+// ICONES IMPORTAÇÃO
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function App() {
+  const [quant, setQuant] = useState(1);
+  const pricePerDrink = 3.90;
+  const totalPrice = quant * pricePerDrink;
+
+  const moreProduct = () => {
+    setQuant(prevQuant => prevQuant + 1);
+    console.log(totalPrice);
+  }
+
+  const lessProduct = () => {
+    if (quant > 1) {
+      setQuant(prevQuant => prevQuant - 1);
+    }
+  }
+
   return (
 
     // BACKGROUND DO APP
@@ -36,6 +57,7 @@ export default function App() {
 
       <View style={{
         position: "absolute",
+
         bottom: 0,
         left: 0,
         width: "100%",
@@ -44,116 +66,183 @@ export default function App() {
         borderTopRightRadius: 50,
       }}>
 
+        {/* DIV QUE ENGLOBA TUDO kk */}
 
-        <Text style={{ bottom: 5, color: "black", fontSize: 30, }}>COCA COLA 300ml</Text>
-        <Text style={{ color: "gray", fontStyle: "italic" }}>EDIÇÃO LIMITADA VINGADORES</Text>
+        <View style={{ padding: 30 }}>
 
-        {/* BOTAO DE ADIOCINAR OU DIMINUIR PRODUTOS */}
+          <Text style={{ bottom: 5, color: "black", fontSize: 30, }}>COCA COLA 300ml</Text>
+          <Text style={{ color: "gray", fontStyle: "italic" }}>EDIÇÃO LIMITADA VINGADORES</Text>
 
-        <View style={{
-          position: "absolute",
-          right: 40,
-          top: -40,
-          backgroundColor: "gray",
-          width: 50,
-          justifyContent: "center",
-          alignItems: "center",
-          padding: 5,
-          borderRadius: 25,
-          gap: 10,
-        }}>
+          {/* BOTAO DE ADIOCINAR OU DIMINUIR PRODUTOS */}
 
-          <TouchableOpacity style={{
-            width: 20,
-            height: 20,
+          <View style={{
+            position: "absolute",
+            right: 40,
+            top: -40,
             backgroundColor: "gray",
-            borderRadius: 20,
+            width: 50,
+            justifyContent: "center",
             alignItems: "center",
-            justifyContent: "center",
-          }}>
-
-            <Text style={{ color: "black" }}> + </Text>
-
-          </TouchableOpacity>
-
-          <View style={{ backgroundColor: "#c21313", height: 30, width: 30, borderRadius: 30, justifyContent: "center", alignItems: "center" }}>
-            <Text style={{
-              color: "black",
-            }}> 2 </Text>
-          </View>
-          <TouchableOpacity style={{
-            width: 20,
-            height: 20,
-            backgroundColor: "gray",
-            borderRadius: 20,
-            alignItems: "center",
-            justifyContent: "center",
-          }}>
-            <Text style={{ color: "black" }}> - </Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* CATEGORIAS DO PRODUTO */}
-
-        <View style={{ width: "100%", marginTop: 10, gap: 20, flexDirection: "row" }}>
-          <View style={{
             padding: 10,
-            borderWidth: 1,
-            borderColor: "#c21313",
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center"
+            borderRadius: 25,
+            gap: 10,
           }}>
-            <Text style={{ color: "#c21313" }}>0,0001%</Text>
-            <Text style={{ color: "#c21313" }}>Bondoso</Text>
+
+            <TouchableOpacity style={{
+              width: 20,
+              height: 20,
+              backgroundColor: "gray",
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+              fontWeight: 20,
+            }} onPress={moreProduct}>
+
+              <Text style={{ color: "white" }}> + </Text>
+            </TouchableOpacity>
+
+            <View style={{
+              backgroundColor: "#c21313",
+              height: 30,
+              width: 30,
+              borderRadius: 30,
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: 20,
+            }}>
+              <Text style={{
+                color: "white",
+              }}> {quant} </Text>
+            </View>
+
+            <TouchableOpacity style={{
+              width: 20,
+              height: 20,
+              backgroundColor: "gray",
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+              fontWeight: 20,
+            }} onPress={lessProduct}>
+              <Text style={{ color: "white" }}> - </Text>
+            </TouchableOpacity>
           </View>
 
-          <View style={{
-            padding: 10,
-            borderWidth: 1,
-            borderColor: "#c21313",
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-            <Text style={{ color: "#c21313" }}>99,9999%</Text>
-            <Text style={{ color: "#c21313" }}>Louco</Text>
+          {/* CATEGORIAS DO PRODUTO */}
+
+          <View style={{ width: "100%", marginTop: 10, gap: 20, flexDirection: "row" }}>
+            <View style={{
+              padding: 10,
+              borderWidth: 1,
+              borderColor: "#c21313",
+              borderRadius: 10,
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <Text style={{ color: "#c21313" }}>0,0001%</Text>
+              <Text style={{ color: "#c21313" }}>Bondoso</Text>
+            </View>
+
+            <View style={{
+              padding: 10,
+              borderWidth: 1,
+              borderColor: "#c21313",
+              borderRadius: 10,
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <Text style={{ color: "#c21313" }}>99,9999%</Text>
+              <Text style={{ color: "#c21313" }}>Louco</Text>
+            </View>
+
+            <View style={{
+              padding: 10,
+              borderWidth: 1,
+              borderColor: "#c21313",
+              borderRadius: 10,
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <Text style={{ color: "#c21313" }}>Infinitamente</Text>
+              <Text style={{ color: "#c21313" }}>Foda</Text>
+            </View>
           </View>
 
-          <View style={{
-            padding: 10,
-            borderWidth: 1,
-            borderColor: "#c21313",
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-            <Text style={{ color: "#c21313" }}>ꝏ</Text>
-            <Text style={{ color: "#c21313" }}>Foda</Text>
-          </View>
-        </View>
+          {/* PREÇO DO PRODUTO UNICO E A QUANTIDADE QUE ESTA NO BOTAO DE AUMENTAR E DIMINUIR O PRODUTO */}
 
-        {/* PREÇO DO PRODUTO UNICO E A QUANTIDADE QUE ESTA NO BOTAO DE AUMENTAR E DIMINUIR O PRODUTO */}
+          <View style={{ width: "100%", gap: 20, flexDirection: "row", marginTop: 20, justifyContent: "center" }}>
+            <View style={{ width: "40%", backgroundColor: "#c21313", padding: 15, borderRadius: 10 }}>
+              <Text style={{ color: "white" }}>A unidade R$3,90</Text>
+            </View>
 
-        <View style={{ width: "100%", gap: 20, flexDirection: "row", marginTop: 20, justifyContent: "center" }}>
-          <View style={{ width: "40%", backgroundColor: "#c21313", padding: 15, borderRadius: 10 }}>
-            <Text style={{ color: "white" }}>R$3,90 a unidade</Text>
+            <View style={{ width: "40%", backgroundColor: "#c21313", padding: 15, borderRadius: 10 }}>
+              <Text style={{ color: "white" }}>{quant} unidade(s) R${totalPrice.toFixed(2)} <Text style={{ color: "white" }}>reais</Text> </Text>
+            </View>
           </View>
 
-          <View style={{ width: "40%", backgroundColor: "#c21313", padding: 15, borderRadius: 10 }}>
-            <Text style={{ color: "white" }}>R$7,80 reais <Text style={{ color: "white" }}>2 unidades</Text> </Text>
-          </View>
         </View>
 
         {/* BOTAO NA QUAL FALA O TOTAL! */}
 
-        <View style={{ flex: 1, backgroundColor: "gray", padding: 30 }}>
-          <Text style={{ color: "black", fontSize: 18, marginTop: 20, }}>Total</Text>
+        <View style={{ flex: 1, backgroundColor: "#c21313", paddingLeft: 30 }}>
+          <Text style={{ color: "white", fontSize: 18, marginTop: 20, fontWeight: 30 }}> PEDIDO TOTAL</Text>
+
+          {/* OS ELEMENTOS QUE ENGLOBLA ESSE "CARRINHO" */}
+
+          <View style={{
+            width: "60%",
+            paddingTop: 20,
+            flexDirection: "row",
+            gap: 20,
+            justifyContent: 'space-evenly',
+          }}>
+
+            <View style={{
+              alignItems: "center",
+              width: 90,
+              height: 50,
+              justifyContent: "flex-end"
+            }}>
+              <Entypo name="drink" size={24} color="white" />
+              <Text style={{ color: "white", fontSize: 12 }}> TOTAL DO CARRINHO </Text>
+            </View>
+
+            {/* PREÇO TOTAL */}
+
+            <View style={{
+              alignItems: "center",
+              width: 90,
+              height: 50,
+              justifyContent: "flex-end"
+            }}>
+              <Text style={{ color: "white", fontSize: 17, }}> R${totalPrice.toFixed(2)} </Text>
+              <Text style={{ color: "white" }}> Preço Total </Text>
+            </View>
+
+          </View>
+
+          <View style={{
+            position: "absolute",
+            right: 60,
+            top: 20,
+            backgroundColor: "gray",
+            width: 60,
+            height: 110,
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 10,
+            borderRadius: 25,
+            gap: 10
+          }}>
+            <Entypo name="credit-card" size={24} color="white" />
+            <Text style={{ color: "black", fontSize: 9.9 }}>MASTERCARD</Text>
+            <Text style={{ color: "white" }}>Pagar</Text>
+          </View>
+
         </View>
 
-
       </View>
-    </View>
+    </View >
   );
 }
 
